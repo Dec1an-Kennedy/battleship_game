e7c7e7240598b1ce155747ba431d6a0aac9a8ced
Dec1an-Kennedy <kennedydeclan543@gmail.com>
2023-01-23 03:10:55 +0000

    def create ships and ship location inputs

--- battleship.py
+++ battleship.py
@@ -23,3 +23,22 @@ letters_to_numbers = {
     'G': 6,
     'H': 7
 }
+
+#computer creates 10 ships
+def create_ships(board):
+    for ship in range(10):
+        ship_row, ship_column = randint(0,7), randint(0,7)
+        while board[ship_row][ship_column] == "X":
+            ship_row, ship_column = get_ship_location()
+        board[ship_row][ship_column] = "X"
+
+        def get_ship_location():
+            row = input("Enter what row you would like to hit: ").upper()
+            while row not in "12345678":
+                print('Not a valid choice, please try selecting another row between 1-8: ').upper()
+                row = input("Enter what row you would like to hit: ").upper()
+                column = input("Enter what column you would like to hit: ").upper()
+                while column not in "ABCDEFGH":
+                    print('Not a valid choice, please try selecting another column between A-H: ')
+                    column = input("Enter what column you would like to hit: ").upper()
+                    return int(row) - 1, letters_to_numbers[column]
