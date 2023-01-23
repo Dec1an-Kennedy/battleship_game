5846058c8ea6fc129433af968843e63c56f277d2
Dec1an-Kennedy <kennedydeclan543@gmail.com>
2023-01-23 02:59:41 +0000

    def print board

--- battleship.py
+++ battleship.py
@@ -4,3 +4,22 @@ from random import randint
 hidden_board = [[" "] * 8 for x in range(8)]
 #Board for displaying hits and misses
 guess_board = [[" "] * 8 for i in range(8)]
+
+def print_board(board):
+    print("  A B C D E F G H")
+    print("  +-+-+-+-+-+-+-+")
+    row_number = 1
+    for row in board:
+        print("%d|%s|" % (row_number, "|".join(row)))
+        row_number += 1
+
+letters_to_numbers = {
+    'A': 0,
+    'B': 1,
+    'C': 2,
+    'D': 3, 
+    'E': 4,
+    'F': 5,
+    'G': 6,
+    'H': 7
+}
